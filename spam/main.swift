8f6ee78f7f5cc85c1a3f50c336240bce039c5c6d
Andrew Clissold <andrewclissold@gmail.com>
2015-04-10 21:52:15 -0400

    Refactor subcommands into individual functions

--- spam/main.swift
+++ spam/main.swift
@@ -1,15 +1,35 @@
+// MARK: main function
+
+func main(repository: String) {
+    if contains(Process.arguments, "install") {
+        install(repository)
+    } else if contains(Process.arguments, "uninstall") {
+        uninstall()
+    } else {
+        usage()
+    }
+}
+
+// MARK: subcommands
+
+func install(repository: String) {
+    mkdir(".spam")
+    call("git", "clone", repository, ".spam/test")
+}
+
+func uninstall() {
+    call("rm", "-rf", ".spam")
+}
+
+// MARK: entry point
+
 let path = "example.swift"
 if let streamReader = StreamReader(path: path) {
     if let line = streamReader.nextLine() {
         if let repository = repo(line) {
-            mkdir(".spam")
-            if (contains(Process.arguments, "install")) {
-                call("git", "clone", repository, ".spam/test")
-            } else if (contains(Process.arguments, "uninstall")) {
-                call("rm", "-rf", ".spam")
-            } else {
-                usage()
-            }
+
+            main(repository)
+
         } else {
             error("could not parse repository name")
         }
