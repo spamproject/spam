7b4240b9162062e0a1a7263bab4dd07c7d350602
Andrew Clissold <andrewclissold@gmail.com>
2015-04-11 20:14:53 -0400

    Install all repos in file; refactor

--- spam/main.swift
+++ spam/main.swift
@@ -1,20 +1,22 @@
-// MARK: main function
-
-func main(repository: String) {
-    if contains(Process.arguments, "install") {
-        install(repository)
-    } else if contains(Process.arguments, "uninstall") {
-        uninstall()
-    } else {
-        usage()
-    }
-}
-
 // MARK: subcommands
 
-func install(repository: String) {
-    mkdir(".spam")
-    call("git", "clone", repository, ".spam/test")
+func install() {
+    func install(repository: String) {
+        mkdir(".spam")
+        call("git", "clone", repository, ".spam/test")
+    }
+
+    let path = "example.swift"
+    if let streamReader = StreamReader(path: path) {
+        var line: String?
+        while let line = streamReader.nextLine() {
+            if let repository = repo(line) {
+                install(repository)
+            }
+        }
+    } else {
+        error("could not read \(path)")
+    }
 }
 
 func uninstall() {
@@ -23,19 +25,10 @@ func uninstall() {
 
 // MARK: entry point
 
-let path = "example.swift"
-if let streamReader = StreamReader(path: path) {
-    if let line = streamReader.nextLine() {
-        if let repository = repo(line) {
-
-            main(repository)
-
-        } else {
-            error("could not parse repository name")
-        }
-    } else {
-        error("\(path) was empty")
-    }
+if contains(Process.arguments, "install") {
+    install()
+} else if contains(Process.arguments, "uninstall") {
+    uninstall()
 } else {
-    error("could not read \(path)")
+    usage()
 }
