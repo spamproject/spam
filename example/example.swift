094f24f200afe7ec8bccc630322d0f4174ee5587
Andrew Clissold <andrewclissold@gmail.com>
2015-04-15 20:38:47 -0400

    Update example to show multiple modules

--- example/example.swift
+++ example/example.swift
@@ -1,5 +1,12 @@
-import Module // aclissold/Module
+import Module        // aclissold/Module
+import AnotherModule // aclissold/AnotherModule
 
+// Module
 let apple = Apple(type: .GrannySmith)
 apple.eat()
 apple.eat()
+
+// AnotherModule
+let pi = Pi()
+pi.eat()
+println(pi)
