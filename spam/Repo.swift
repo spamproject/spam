00a439d4df6984dbac56a972e07ea324fc074374
Andrew Clissold <andrewclissold@gmail.com>
2015-04-16 18:13:54 -0400

    Improve regex granularity

--- spam/Repo.swift
+++ spam/Repo.swift
@@ -13,14 +13,16 @@ class Repo {
     }
 
     convenience init?(importStatement: String) {
-        if let nameRange = importStatement.rangeOfString("[^\\s/]+/[^\\s/]+$",
-            options: .RegularExpressionSearch) {
-                let name = importStatement.substringWithRange(nameRange)
-                let components = name.componentsSeparatedByString("/")
-                if components.count == 2 {
-                    self.init(username: components[0], reponame: components[1])
-                    return
-                }
+        if importStatement.rangeOfString("import",
+        options: .RegularExpressionSearch) != nil,
+        let nameRange = importStatement.rangeOfString("[^\\s/]+/[^\\s/]+$",
+        options: .RegularExpressionSearch) {
+            let name = importStatement.substringWithRange(nameRange)
+            let components = name.componentsSeparatedByString("/")
+            if components.count == 2 {
+                self.init(username: components[0], reponame: components[1])
+                return
+            }
         }
 
         self.init(username: "", reponame: "")
