161d33f828d8740770c37281c95ed4880adc9bed
Andrew Clissold <andrewclissold@gmail.com>
2015-04-15 20:38:25 -0400

    Fix compilation errors

--- spam/main.swift
+++ spam/main.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 let spamDirectory = ".spam"
+let swiftc = "xcrun -sdk macosx swiftc"
 
 private extension Repo {
     var installPath: String {
@@ -44,7 +45,7 @@ func compile(#modules: [String], mainFile: String = "main.swift") {
     mkdir("\(s)/build")
     mkdir("\(s)/lib")
 
-    var command = "swiftc -I \(s)/lib -L \(s)/lib "
+    var command = "\(swiftc) -I \(s)/lib -L \(s)/lib "
     for module in modules {
         compile(module)
         command += "-l\(module.lowercaseString) "
@@ -58,11 +59,11 @@ func compile(moduleName: String) {
     let m = moduleName.lowercaseString
     let s = spamDirectory
 
-    call("swiftc -emit-library -emit-object \(s)/aclissold/\(M)/\(M).swift " +
+    call("\(swiftc) -emit-library -emit-object \(s)/aclissold/\(M)/\(M).swift " +
          "-module-name \(M) -o \(s)/build/\(M).o")
     call("ar rcs lib\(m).a \(s)/build/\(M).o")
     call("mv lib\(m).a \(s)/lib/")
-    call("swiftc -emit-module \(s)/aclissold/\(M)/\(M).swift " +
+    call("\(swiftc) -emit-module \(s)/aclissold/\(M)/\(M).swift " +
          "-module-name \(M) -o \(s)/lib/")
 }
 
@@ -73,7 +74,7 @@ if contains(Process.arguments, "install") || contains(Process.arguments, "i") {
 } else if contains(Process.arguments, "uninstall") || contains(Process.arguments, "u") {
     uninstall()
 } else if contains(Process.arguments, "compile") || contains(Process.arguments, "c") {
-    compile(modules: ["Module"], mainFile: "example.swift")
+    compile(modules: ["Module", "AnotherModule"], mainFile: "example.swift")
 } else {
     usage()
 }
